efde8961433d454a0c1c881f9deb5e5247ae3e51
snamiki1212 <snamiki1212@gmail.com>
2022-01-23 12:24:55 -0800

    feat: add integer literal feature

--- ast/ast.go
+++ ast/ast.go
@@ -80,6 +80,18 @@ func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
 ///
+/// Literal
+///
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+///
 /// Return
 ///
 type ReturnStatement struct {
