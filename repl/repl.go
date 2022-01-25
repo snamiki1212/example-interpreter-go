e89f6a26633e99f63871469128e6dd4cec8244c4
snamiki1212 <snamiki1212@gmail.com>
2022-01-24 23:42:33 -0800

    feat: enhance pretty error message

--- repl/repl.go
+++ repl/repl.go
@@ -11,6 +11,19 @@ import (
 
 const PROMPT = ">> "
 
+const MONKEY_FACE = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
+
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
@@ -38,6 +51,10 @@ func Start(in io.Reader, out io.Writer) {
 }
 
 func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+
 	for _, msg := range errors {
 		io.WriteString(out, "\t"+msg+"\n")
 	}
