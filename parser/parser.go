c57d4366a71cab4fc5617c54a9028d20faf2afb1
snamiki1212 <snamiki1212@gmail.com>
2022-01-24 22:25:02 -0800

    feat: add if-else parser

--- parser/parser.go
+++ parser/parser.go
@@ -59,6 +59,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
 
 	// setup infix
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
@@ -83,7 +84,7 @@ func (p *Parser) Errors() []string {
 }
 
 ///
-/// for Statement
+/// for GroupedExpression
 ///
 func (p *Parser) parseGroupedExpression() ast.Expression {
 	p.nextToken()
@@ -92,6 +93,52 @@ func (p *Parser) parseGroupedExpression() ast.Expression {
 		return nil
 	}
 	return exp
+}
+
+///
+/// for If
+///
+func (p *Parser) parseIfExpression() ast.Expression {
+	expression := &ast.IfExpression{Token: p.curToken}
+	if !p.expectPeek(token.LPAREN) { // NOTE: (
+		return nil
+	}
+	p.nextToken()
+	expression.Condition = p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) { // NOTE: )
+		return nil
+	}
+	if !p.expectPeek(token.LBRACE) { // NOTE: {
+		return nil
+	}
+	expression.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken() // skip 'else'
+		if !p.expectPeek(token.LBRACE) {
+			return nil
+		}
+		expression.Alternative = p.parseBlockStatement()
+	}
+	return expression
+}
+
+///
+/// for BlockStatement
+///
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+	p.nextToken()
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			block.Statements = append(block.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return block
 }
 
 ///
