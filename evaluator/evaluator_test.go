be2da78e1b574a98a6005e3539714f79defca485
snamiki1212 <snamiki1212@gmail.com>
2022-01-29 09:21:27 -0800

    feat: add return-val feature

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,6 +8,33 @@ import (
 	"monkey/parser"
 )
 
+func TestReturnStatements(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"return 10;", 10},
+		{"return 10; 9;", 10},
+		{"return 2 * 5; 9;", 10},
+		{"9; return 2 * 5; 9;", 10},
+
+		// nest
+		{`
+		if (10 > 1) {
+			if(10 > 1) {
+				return 10;
+			}
+			return 1;
+		}
+		`, 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
 func TestIfEleseExpression(t *testing.T) {
 	tests := []struct {
 		input    string
