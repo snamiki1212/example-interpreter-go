81742dfaabfb1067087f466f073f3cf5f18a36c5
snamiki1212 <snamiki1212@gmail.com>
2022-01-28 19:41:02 -0800

    refactor: update memory usage

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -5,6 +5,12 @@ import (
 	"monkey/object"
 )
 
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
+)
+
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
 
@@ -21,7 +27,7 @@ func Eval(node ast.Node) object.Object {
 
 	// boolean
 	case *ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return naiveBoolToBooleanObject(node.Value)
 	}
 
 	return nil
@@ -33,4 +39,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
 		result = Eval(statement)
 	}
 	return result
+}
+
+func naiveBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
