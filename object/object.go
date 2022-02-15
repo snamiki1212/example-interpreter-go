e3f43b15c4d22153a0f1f4acf429f537f81f2e81
snamiki1212 <snamiki1212@gmail.com>
2022-02-15 14:20:47 -0800

    feat: add string evaluator

--- object/object.go
+++ object/object.go
@@ -16,12 +16,23 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
+	STRING_OBJ       = "STRING"
 )
 
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+///
+/// String
+///
+type String struct {
+	Value string
+}
+
+func (s *String) Type() ObjectType { return STRING_OBJ }
+func (s *String) Inspect() string  { return s.Value }
 
 ///
 /// Integer
